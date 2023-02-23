326b23825dcbe04ccfe53a6ccad22053413b319b
Ilyaz22 <so.ilia@yandex.ru>
2023-02-24 00:54:03 +0300

    add 68-4

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -28,14 +28,38 @@ RSpec.describe GameQuestion, type: :model do
       # Именно под буквой b в тесте мы спрятали указатель на верный ответ
       expect(game_question.answer_correct?('b')).to be_truthy
     end
-  end
 
-  # help_hash у нас имеет такой формат:
-  # {
-  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
-  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
-  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
-  # }
+    # тест на наличие методов делегатов level и text
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
+    end
+
+    # help_hash у нас имеет такой формат:
+    # {
+    #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
+    #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
+    #   friend_call: 'Василий Петрович считает, что правильный ответ A'
+    # }
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
+  end
 
   context 'user helpers' do
     it 'correct audience_help' do
@@ -50,15 +74,38 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
-  # тест на наличие методов делегатов level и text
-  it 'correct .level & .text delegates' do
-    expect(game_question.text).to eq(game_question.question.text)
-    expect(game_question.level).to eq(game_question.question.level)
+  it 'correct fifty_fifty' do
+    expect(game_question.help_hash).not_to include(:fifty_fifty)
+    game_question.add_fifty_fifty
+
+    # проверим создание подсказки
+    expect(game_question.help_hash).to include(:fifty_fifty)
+    ff = game_question.help_hash[:fifty_fifty]
+
+    expect(ff).to include('b') # должен остаться правильный вариант
+    expect(ff.size).to eq 2 # всего должно остаться 2 варианта
   end
 
   describe '#correct_answer_key' do
     it 'returns correct answer key' do
       expect(game_question.correct_answer_key).to eq('b')
+    end
+  end
+
+  describe '#add_friend_call' do
+    before { game_question.add_friend_call }
+    let!(:call) { game_question.help_hash[:friend_call] }
+
+    it 'help_hash includes fifty_fifty' do
+      expect(game_question.help_hash).to include(:friend_call)
+    end
+
+    it 'return string' do
+      expect(call).to be_a(String)
+    end
+
+    it 'contains answer key' do
+      expect(call).to include('A').or include('B').or include('C').or include('D')
     end
   end
 end
